a9c9b43236ecd619664bed7400c296b2a86753d4
Laury Baillon <laury-baillon@hotmail.fr>
2026-01-21 17:36:53 +0100

    set up of filter visuals + logic for product availability

--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -4,12 +4,18 @@ import { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import styles from "./Products.module.css";
 import type { MenuProps } from "antd";
-import { Button, Dropdown } from "antd";
+import { Button, Dropdown, Empty, Slider } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 
 type Product = {
+    id: number;
     title: string;
     photos: string[];
+    theme: string;
+    type: string;
+    value: number;
+    purchaseOption: boolean;
+    available: boolean;
 };
 
 type Artist = {
@@ -18,6 +24,7 @@ type Artist = {
 };
 
 function Products() {
+    const [allProducts, setAllProducts] = useState<Product[]>([]);
     const [productsData, setProductsData] = useState<Product[]>([]);
     const [artistList, setArtistList] = useState<Artist[]>([]);
 
@@ -26,10 +33,9 @@ function Products() {
             .then((response) => response.json())
             .then((data) => {
                 setProductsData(data);
+                setAllProducts(data)
             });
-    }, []);
 
-    useEffect(() => {
         fetch("http://localhost:9000/artists")
             .then((response) => response.json())
             .then((data) => {
@@ -37,15 +43,19 @@ function Products() {
             });
     }, []);
 
-    const products = productsData.map((data, i) => {
-        return (
-            <img
-                key={i}
-                className={styles.productImage}
-                alt={data.title}
-                src={data.photos[0]}
-            ></img>
-        );
+    const products = productsData.map((data) => {
+        if (productsData.length === 0) {
+            return <Empty description="Aucun produit trouvé" />;
+        } else {
+            return (
+                <img
+                    key={data.id}
+                    className={styles.productImage}
+                    alt={data.title}
+                    src={data.photos[0]}
+                ></img>
+            );
+        }
     });
 
     // const filterTable = [
@@ -61,31 +71,39 @@ function Products() {
     const availability: MenuProps["items"] = [
         {
             label: "disponible",
-            key: "1",
+            key: "available",
         },
         {
             label: "non disponible",
-            key: "2",
+            key: "unavailable",
         },
     ];
 
     const availabilityClick: MenuProps["onClick"] = (e) => {
-        console.log("dipo?", e);
+        const filteredProducts = allProducts.filter((product) => {
+            if (e.key === "available") {
+                return product.available;
+            }
+            if (e.key === "unavailable") {
+                return !product.available;
+            }
+        });
+
+        setProductsData(filteredProducts);
     };
 
     const availabilityProps = {
         items: availability,
         onClick: availabilityClick,
     };
-    
-    
+
     const artistFilter: MenuProps["items"] = artistList.map((artist) => ({
         label: artist.name,
         key: artist.id.toString(),
     }));
 
     const artistClick: MenuProps["onClick"] = (e) => {
-        console.log("artiste clické", e);
+        console.log("artiste cliqué", e);
     };
 
     const artistProps = {
@@ -93,10 +111,110 @@ function Products() {
         onClick: artistClick,
     };
 
-    //pour ne pas afficher 0 produits
-    if (products.length === 0) {
-        return null;
-    }
+    const themeFilter: MenuProps["items"] = productsData
+        .reduce((accumulator: string[], product) => {
+            if (!accumulator.includes(product.theme)) {
+                accumulator.push(product.theme);
+            }
+            return accumulator;
+        }, [])
+        .map((theme: string) => ({
+            label: theme,
+            key: theme,
+        }));
+
+    const themeClick: MenuProps["onClick"] = (e) => {
+        console.log("thème cliqué", e);
+    };
+
+    const themeProps = {
+        items: themeFilter,
+        onClick: themeClick,
+    };
+
+    const typeFilter: MenuProps["items"] = productsData
+        .reduce((accumulator: string[], product) => {
+            if (!accumulator.includes(product.type)) {
+                accumulator.push(product.type);
+            }
+            return accumulator;
+        }, [])
+        .map((type: string) => ({
+            label: type,
+            key: type,
+        }));
+
+    const typeClick: MenuProps["onClick"] = (e) => {
+        console.log("type cliqué", e);
+    };
+
+    const typeProps = {
+        items: typeFilter,
+        onClick: typeClick,
+    };
+
+    const buyable: MenuProps["items"] = [
+        {
+            label: "achat possible",
+            key: "buyable",
+        },
+        {
+            label: "indisponible à l'achat",
+            key: "unbuyable",
+        },
+    ];
+
+    const buyableClick: MenuProps["onClick"] = (e) => {
+        console.log("dipo à l'achat ?", e);
+    };
+
+    const buyableProps = {
+        items: buyable,
+        onClick: buyableClick,
+    };
+
+    const maxValue: number = productsData.reduce(
+        (accumulator: number, product) => {
+            if (accumulator < product.value) {
+                accumulator = product.value;
+            }
+            return accumulator;
+        },
+        0
+    );
+
+    const minValue: number = productsData.reduce(
+        (accumulator: number, product) => {
+            if (accumulator > product.value) {
+                accumulator = product.value;
+            }
+            return accumulator;
+        },
+        maxValue
+    );
+
+    const priceFilter: MenuProps["items"] = [
+        {
+            label: (
+                <Slider
+                    range={{ draggableTrack: true }}
+                    max={maxValue}
+                    min={minValue}
+                    defaultValue={[minValue, maxValue]}
+                />
+            ),
+            key: "1",
+        },
+    ];
+
+    const priceClick: MenuProps["onClick"] = (e) => {
+        console.log("prix choisi", e);
+    };
+
+    const priceProps = {
+        items: priceFilter,
+        onClick: priceClick,
+    };
 
     return (
         <div className={styles.main}>
@@ -115,13 +233,57 @@ function Products() {
                 <div className={styles.filterAndProductContainer}>
                     <div className={styles.filterContainer}>
                         <Dropdown menu={availabilityProps}>
-                            <Button icon={<DownOutlined />} iconPlacement="end" className={styles.filterButton}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
                                 disponibilité
                             </Button>
                         </Dropdown>
                         <Dropdown menu={artistProps}>
-                            <Button icon={<DownOutlined />} iconPlacement="end" className={styles.filterButton}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
                                 artistes
+                            </Button>
+                        </Dropdown>
+                        <Dropdown menu={themeProps}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
+                                Thème
+                            </Button>
+                        </Dropdown>
+                        <Dropdown menu={typeProps}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
+                                Type d'oeuvre
+                            </Button>
+                        </Dropdown>
+                        <Dropdown menu={buyableProps}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
+                                Possibilité d'achat
+                            </Button>
+                        </Dropdown>
+                        <Dropdown menu={priceProps}>
+                            <Button
+                                icon={<DownOutlined />}
+                                iconPlacement="end"
+                                className={styles.filterButton}
+                            >
+                                Valeur de l'oeuvre
                             </Button>
                         </Dropdown>
                     </div>
